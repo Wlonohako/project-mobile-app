12599442d9ce781708a28bf0ff47aebe2a22c1dd
Wojciech Sowa <w.sowa6@gmail.com>
2024-12-17 19:44:32 +0100

    add project

---
+++ app/src/main/java/com/example/workplease/MainForm.kt
@@ -0,0 +1,66 @@
+package com.example.workplease
+
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
+import android.widget.Toast
+import com.google.gson.Gson
+
+class MainForm : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_form)
+
+        val saveButton = findViewById<Button>(R.id.saveButton)
+        val screenTwoButton = findViewById<Button>(R.id.ScreenTwoButton)
+
+        screenTwoButton.setOnClickListener {
+            val intent = Intent(this, HumanList::class.java)
+            startActivity(intent)
+        }
+
+        saveButton.setOnClickListener {
+            val nameTextInput = findViewById<EditText>(R.id.nameInput).text.toString()
+            val surnameTextInput = findViewById<EditText>(R.id.surnameInput).text.toString()
+            val ageNumberInput = findViewById<EditText>(R.id.ageInput).text.toString()
+            val highNumberInput = findViewById<EditText>(R.id.highInput).text.toString()
+            val weightNumberInput = findViewById<EditText>(R.id.weightInput).text.toString()
+
+            if (nameTextInput.isEmpty() || surnameTextInput.isEmpty() || ageNumberInput.isEmpty() || highNumberInput.isEmpty() || weightNumberInput.isEmpty()) {
+                Toast.makeText(this, "All fields required", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+            if (ageNumberInput.toInt() < 0) {
+                Toast.makeText(this, "Age must be greater than 0", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+            if (highNumberInput.toInt() !in 50..250) {
+                Toast.makeText(this, "High must be between 50cm and 250cm", Toast.LENGTH_SHORT)
+                    .show()
+                return@setOnClickListener
+            }
+            if (weightNumberInput.toInt() !in 3..200) {
+                Toast.makeText(this, "Weight must be between 3kg and 200kg", Toast.LENGTH_SHORT)
+                    .show()
+                return@setOnClickListener
+            }
+            val dict = mapOf(
+                "name" to nameTextInput,
+                "surname" to surnameTextInput,
+                "age" to ageNumberInput,
+                "high" to highNumberInput,
+                "weight" to weightNumberInput
+            )
+            val json = Gson().toJson(dict)
+            saveSharedPreferences(json)
+
+            Toast.makeText(this, "Saved", Toast.LENGTH_SHORT).show()
+
+
+        }
+    }
+}
---
+++ app/src/main/java/com/example/workplease/SharedPreferences.kt
@@ -0,0 +1,9 @@
+package com.example.workplease
+
+
+const val listName = "femboy"
+
+
+fun saveSharedPreferences(json: String) {
+
+}
